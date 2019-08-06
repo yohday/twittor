f89189d4dbc9872d15bc268e2b2b094d892d6717
Yohanna Sequera <yohasequera@gmail.com>
2019-08-06 15:34:29 -0400

    Cambio ruta SW

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const DINAMIC_CACHE = 'dinamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    './',
+    // './',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
